4b16f510dfa738ceb09c424204fc66261defc1bb
Alex Disdur <alex4mukhin@mail.ru>
2016-05-31 10:25:02 +0600

    trial commit

--- Library_App/models.py
+++ Library_App/models.py
@@ -1,11 +1,9 @@
 from django.db import models
-
 class LibraryEmployee(models.Model):
     name = models.CharField(max_length=30)
     patronymic_name = models.CharField(max_length=30)
     surname = models.CharField(max_length=30)
-    photo = models.ImageField()
-    position = models.CharField(max_lentgh=30)
+    position = models.CharField(max_length=30)
 
 class Reader(models.Model):
     library_card_number = models.IntegerField(primary_key=True)
@@ -33,7 +31,7 @@ class Operation(models.Model):
 class BookCopy(models.Model):
     name = models.CharField(max_length=30)
     year_of_publication = models.IntegerField()
-    amount_of_all_copies = models.IntegerField() 
+    #amount_of_all_copies = models.IntegerField() 
 
 class Author(models.Model):
     name = models.CharField(max_length=30)
--- Library_App/views.py
+++ Library_App/views.py
@@ -8,18 +8,36 @@ from django.views.generic.base import TemplateView
 
 class IndexView(TemplateView):
     template_name = "index.html"
-    #Activity.objects.all().delete()
-    #Participant.objects.all().delete()
-    #Place.objects.all().delete()
-    #Activity.objects.create (id = 1, name = "Подъем", time = "08:00", date = "2014-10-12")
-    #Activity.objects.create (id = 2, name = "Сходить на фильм", time = "16:00", date = "2015-10-12")
-    #Activity.objects.create (id = 3, name = "Подъем", time = "08:00", date = "2015-09-12")
-    #Place.objects.create (id = 1, name = "Кинотеатр Киномакс", city = "Томск", street = "Розы Люксембург", house = "73")
-    #Place.objects.create (id = 2, name = "Администрация г. Томска", city = "Томск", street = "Ленина", house = "73")
-    #Place.objects.create (id = 3, name = "Родильный ном №1", city = "Томск", street = "Ленина", house = "65")
-    #Participant.objects.create (id = 1, first_name = "Павел", second_name = "Павлов", third_name = "Павлович", sex = "М", phone = "89131162233")
-    #Participant.objects.create (id = 2, first_name = "Анастасия", second_name = "Павлова", third_name = "Олеговна", sex = "Ж", phone = "89132162233")
-    #Participant.objects.create (id = 3, first_name = "Илья", second_name = "Ильин", third_name = "Ильич", sex = "М", phone = "+73332221199")
+    LibraryEmployee.objects.create (id = 1, name = "Иван", patronymic_name = "Сергеевич", surname = "Кочин",
+                                    position = "секретарь")
+    LibraryEmployee.objects.create (id = 2, name = "Фёдор", patronymic_name = "Петрович", surname = "Мерков",
+                                    position = "библиотекарь")
+    LibraryEmployee.objects.create (id = 3, name = "Лариса", patronymic_name = "Константиновна", surname = "Никифорова",
+                                    position = "библиотекарь")
+
+    
+    Reader.objects.create (id = 1, library_card_number = "5946354869", name = "Алексей", patronymic_name = "Петрович", surname = "Фандорин",
+                                    passport_number = "1568735195", date_of_birth = "1984-10-12", proup_number = "562")
+    Reader.objects.create (id = 2, library_card_number = "5948351684", name = "Аркадий", patronymic_name = "Львович", surname = "Ночин",
+                                    passport_number = "1368495362", date_of_birth = "1965-05-02", proup_number = "469")
+    Reader.objects.create (id = 3, library_card_number = "4962384679", name = "Августин", patronymic_name = "Эрнестович", surname = "Зубрин",
+                                    passport_number = "5316792645", date_of_birth = "1990-05-22", proup_number = "163")
+
+
+    Operation.objects.create (id = 1, operation_date = "2013-11-19", required_return_date = "2014-05-19", operation_type = "выдача")
+    Operation.objects.create (id = 2, operation_date = "2013-12-25", required_return_date = "2014-06-25", operation_type = "выдача")
+    Operation.objects.create (id = 3, operation_date = "2014-03-05", required_return_date = "2014-10-05", operation_type = "выдача")
+
+    
+    BookCopy.objects.create (id = 1, name = "Beatles' songbook", year_of_publication = "1985", amount_of_all_copies = "2")
+    BookCopy.objects.create (id = 2, name = "Общий курс физики", year_of_publication = "2003", amount_of_all_copies = "20")
+    BookCopy.objects.create (id = 3, name = "Прикладная философия", year_of_publication = "1999", amount_of_all_copies = "15")
+
+
+    Author.objects.create (id = 1, name = "Sergio", patronymic_name = "Сергеевич", surname = "Palumbo", book_copy = '1')
+    Author.objects.create (id = 2, name = "Очень", patronymic_name = "Плохой", surname = "Писатель", book_copy = '2')
+    Author.objects.create (id = 3, name = "Виктор", patronymic_name = "Антонович", surname = "Розгунов", book_copy = '3')
+
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
         context.update(
@@ -28,7 +46,7 @@ class IndexView(TemplateView):
                 'readers_list' : Reader.objects.all(),
                 'operations_list' : Operation.objects.all(),
                 'book_copies_list' : BookCopy.objects.all(),
-                'authors_list' : Author.objects.all(),
+                'authors_list' : Author.objects.all()
             }
         )
         return context
